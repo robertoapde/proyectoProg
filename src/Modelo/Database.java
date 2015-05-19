a9451e67a780706c2e7e806f44c4eccb47b27ae7
Nacho <Nacho@PC-Nacho>
2015-05-19 12:35:53 +0200

    Versión 1905151235

--- src/Modelo/Database.java
+++ src/Modelo/Database.java
@@ -8,12 +8,12 @@ public class Database {
     private  String db = "dam26_dbtest";
     private String user = "dam26";
     private String password = "Mazuecos14";
-    private String url = "jdbc:mysql://192.168.48.2:3306"+db;
+    private String url = "jdbc:mysql://88.26.202.99:3306"+db;
     private Connection conn = null;
 
 
 public Database(){
-    this.url = "jdbc:mysql://192.168.48.2:3306/"+this.db;
+    this.url = "jdbc:mysql://88.26.202.99:3306/"+this.db;
         try{
             Class.forName("com.mysql.jdbc.Driver");
             conn = DriverManager.getConnection( this.url, this.user , this.password );    
@@ -26,6 +26,7 @@ public Database(){
             System.err.println( e.getMessage() );
         } 
     }
+
 public Connection getConexion(){
     return this.conn;
     }
